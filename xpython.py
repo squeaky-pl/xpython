c646ae18455464aa8a44a40b8d472919e8982b42
Squeaky <squeaky_pl@gmx.com>
2017-02-12 20:26:53 -0200

    initial

---
+++ xpython.py
@@ -0,0 +1,73 @@
+import gccjit
+import dis
+
+
+def get_fun_code(source):
+    module = compile(source, 'unknown', 'exec')
+    return module.co_consts[0]
+
+
+def compile_to_context(context, name, code):
+    stack = []
+    variables = []
+    ret = ...
+
+    # setup parameters
+    for i in range(code.co_argcount):
+        variables.append(context.param("int", code.co_varnames[i]))
+
+    func = context.exported_function("int", name, variables)
+
+    # allocate blocks
+    blocks = []
+    block_map = {}
+    current_block_pos = None
+    for instruction in dis.get_instructions(code):
+        if current_block_pos is None:
+            current_block_pos = instruction.offset
+        if instruction.opname in ['RETURN_VALUE', 'POP_JUMP_IF_FALSE']:
+            block = context.block(func)
+            blocks.append(block)
+            block_map[current_block_pos] = block
+            current_block_pos = None
+
+    print(block_map)
+
+    # setup space for locals
+    variables.extend([None] * (code.co_nlocals - code.co_argcount))
+
+    block_iter = iter(blocks)
+    block = next(block_iter)
+
+    for instruction in dis.get_instructions(code):
+        opname = instruction.opname
+        argval = instruction.argval
+        arg = instruction.arg
+        if opname == 'LOAD_CONST':
+            # stack.append(argval)
+            stack.append(context.integer(argval))
+        elif opname == 'STORE_FAST':
+            variables[arg] = stack.pop()
+        elif opname == 'LOAD_FAST':
+            stack.append(variables[arg])
+        elif opname == 'BINARY_ADD':
+            # stack.append(stack.pop() + stack.pop())
+            addition = context.binary('+', "int", stack.pop(), stack.pop())
+            stack.append(addition)
+        elif opname == 'COMPARE_OP':
+            b = stack.pop()
+            a = stack.pop()
+            comparison = context.comparison(dis.cmp_op[arg], a, b)
+            stack.append(comparison)
+        elif opname == 'RETURN_VALUE':
+            block.end_with_return(stack.pop())
+            block = next(block_iter, None)
+            # ret = stack.pop()
+        elif opname == 'POP_JUMP_IF_FALSE':
+            next_block = next(block_iter)
+            block.end_with_conditonal(stack.pop(), next_block, block_map[arg])
+            block = next_block
+        else:
+            assert 0, "Unknown opname " + opname
+
+        print('stack {}, variables {}, ret {}'.format(stack, variables, ret))
