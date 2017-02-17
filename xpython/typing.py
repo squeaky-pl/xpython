5e549602b5fa1b0f3f95d4f6f3fbdfd10f2ed6d1
Squeaky <squeaky_pl@gmx.com>
2017-02-17 00:03:55 -0200

    add struct instances

--- xpython/typing.py
+++ xpython/typing.py
@@ -1,3 +1,6 @@
+from collections import OrderedDict
+
+
 def byte(x):
     assert 0 <= x <= 0xff
 
@@ -9,4 +12,25 @@ def unsigned(x):
 class struct:
     def __init__(self, name, *fields):
         self.name = name
-        self.fields = fields
+        self.fields = OrderedDict(fields)
+
+    def __call__(self, **kwargs):
+        for name in kwargs:
+            assert name in self.fields
+
+        return struct_instance(self, kwargs)
+
+
+class struct_instance:
+    def __init__(self, typ, values):
+        self.__type = typ
+        for k, v in values.items():
+            setattr(self, k, v)
+
+    def __repr__(self):
+        r = '<' + self.__type.name + ' '
+        r += ' '.join(
+            f + '=' + str(getattr(self, f)) for f in self.__type.fields)
+        r += '>'
+
+        return r
